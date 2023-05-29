0b7fd896a505c726624832735a526092925ebd09
GopindaAditya <111219384+GopindaAditya@users.noreply.github.com>
2023-05-29 16:08:13 +0700

    Add files via upload

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,17 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\login;
 use Illuminate\Http\Request;
-use Hash;
-use Session;
-use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Requests\StoreloginRequest;
-use App\Http\Requests\UpdateloginRequest;
 
 class LoginController extends Controller
 {
-    
-}
+    public function showLoginForm()
+    {
+        return view('login');
+    }
 
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            // Autentikasi berhasil
+            // $user = Auth::user();p
+            return redirect()->route('user.dashboard');
+        }else if (Auth::guard('owners')->attempt($credentials)) {
+            return redirect()->route('owner.dashboard');
+        }else if(Auth::guard('kasirs')->attempt($credentials)){
+            return redirect()->route('kasir.dashboard');
+        } else {
+            // Autentikasi gagal
+            return redirect()->back()->withErrors(['email' => 'Email atau kata sandi salah']);
+        }
+    }
+}
