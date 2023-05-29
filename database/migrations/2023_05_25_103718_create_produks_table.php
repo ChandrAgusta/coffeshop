a5cfacddd7ffd74800a1344a5a2a11acc0aa5d1c
GopindaAditya <111219384+GopindaAditya@users.noreply.github.com>
2023-05-29 16:20:41 +0700

    Add files via upload

---
+++ database/migrations/2023_05_25_103718_create_produks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProduksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('harga');            
+            $table->string('desc');
+            $table->enum('kategori', ['Baverages', 'Latte','Cappucino','Cold Brew']);
+            $table->string('linkFoto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produks');
+    }
+}
